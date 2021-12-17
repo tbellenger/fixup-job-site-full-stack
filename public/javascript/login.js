233fbbaf4cfea607dc9a807f81495b83bc6b53cd
tbellenger <tbellenger@gmail.com>
2021-12-17 10:04:19 -0800

    adding login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,14 @@
 const signupInsteadBtn = document.querySelector("#signup-instead");
 const loginInsteadBtn = document.querySelector("#login-instead");
+const loginBtn = document.querySelector("#login");
+const signupBtn = document.querySelector("#signup");
+const usernameEl = document.querySelector("#username-login");
+const passwordEl = document.querySelector("#password-login");
 
 signupInsteadBtn.addEventListener("click", toggleSignupLogin);
 loginInsteadBtn.addEventListener("click", toggleSignupLogin);
+loginBtn.addEventListener("click", login);
+signupBtn.addEventListener("click", signup);
 
 function toggleSignupLogin() {
   event.preventDefault();
@@ -13,3 +19,24 @@ function toggleSignupLogin() {
   loginFormEl.classList.toggle("hide");
   signupFormEl.classList.toggle("hide");
 }
+
+async function login() {
+  const response = await fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: {
+      email: usernameEl.value().trim(),
+      password: passwordEl.value().trim(),
+    },
+  });
+  const json = await response.json();
+  if (json) {
+    if (json.token) {
+      localStorage.setItem("token", json.token);
+    }
+  }
+}
+
+function signup() {}
