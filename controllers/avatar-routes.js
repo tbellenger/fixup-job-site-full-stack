9db2ffe319a09499b7072b9ab5e9ac1f96025e7a
tbellenger <tbellenger@gmail.com>
2021-12-19 12:08:25 -0800

    no id on avatar -id is string

--- controllers/avatar-routes.js
+++ controllers/avatar-routes.js
@@ -15,7 +15,7 @@ router.get("/:id", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     res.setHeader("Content-Type", "image/png");
-    const buffer = identicon.generateSync({ id: 999999, size: 40 });
+    const buffer = identicon.generateSync({ id: "999999", size: 40 });
     res.end(buffer);
   } catch (err) {
     res.status(500).json(err);
