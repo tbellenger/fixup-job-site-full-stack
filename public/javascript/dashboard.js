51e1100d8285bdfabcbd90eb6752c3bcebb2564d
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-18 22:17:08 -0800

    remove unnecessary line of codes

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -7,8 +7,7 @@ async function newFormHandler(event) {
     const job_location = document.querySelector('input[name="job-location"]').value.trim();
     const payment_method = document.querySelector('input[name="payment-method"]').value.trim();
     const category_name = document.querySelector('input[name="category-name"]').value.trim();
-    const user_id = document.querySelector(".logged-in-user_id").innerHTML;
-    
+   
 
     if(!username){
       alert("Please login or signup to create post.")
@@ -37,38 +36,12 @@ async function newFormHandler(event) {
   }
 };
 
-//delete post from client to API
-const deletePostHandler = async (event) => {
-    event.preventDefault();
-
-    const deletePostId = event.target.getAttribute("data-id");
-    if (deletePostId) {
-        const response = await fetch("/api/job/" + deletePostId, {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json" },
-        });
-        if (response.ok) {
-            document.location.replace("/dashboard");
-        } else {
-            alert(
-                "Failed to delete post. " +
-                    response.status +
-                    ": " +
-                    response.statusText
-            );
-        }
-    }
-};
 
 //add event listeners
 document
     .querySelector(".submit-post")
-    .addEventListener("click", submitPostHandler);
+    .addEventListener("click", newFormHandler);
 
-const deleteButtons = document.querySelectorAll(".delete-post");
-deleteButtons.forEach((el) =>
-    el.addEventListener("click", (event) => deletePostHandler(event))
-);
 
  
   
